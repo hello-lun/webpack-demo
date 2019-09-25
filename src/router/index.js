f1c2aec5c0f21792cab0e02b225cc5110e4dd58c
dinglun <dinglun@yy.com>
2019-09-25 19:32:40 +0800

    手动

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,8 @@ const router = new vueRouter({
             path: '/info',
             component: () => import(/* webpackChunkName: "group-foo" */ '../views/home/index.vue'),
         },
+        { path: '/tree', component: () => import(/* webpackChunkName: "group-nothing" */ '../views/tree/index.vue'), },
+
         { path: '/', component: () => import(/* webpackChunkName: "group-nothing" */ '../views/index/index.vue'), },
 
         { path: '*', component: () => import(/* webpackChunkName: "group-indexHome" */ '../views/nothing/index.vue'), }
---
+++ src/views/tree/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="app" id="app">
+    <VueTree :tree-data="treeData2" :tree-click-event="treeClickEvent"></VueTree>
+  </div>
+</template>
+
+<script>
+import VueTree from "./Tree.vue";
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      treeData2: [
+        {
+          text: "一级", // 显示的文字
+          expand: false, // 默认是否展开
+          children: [
+            {
+              text: "二级-1",
+              expand: false,
+            },
+            {
+              text: "二级-2",
+              expand: false,
+              children: [
+                {
+                  text: "三级-1",
+                  expand: false,
+                },
+                {
+                  text: "三级-2",
+                  expand: false,
+                  children: [
+                    {
+                      text: "四级-1",
+                      expand: false,
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          text: "一级-2",
+          expand: false
+        }
+      ]
+    }
+  },
+  methods: {
+    treeClickEvent(item, treeItem){
+      console.log(item);
+    }
+  },
+  components: {
+    VueTree
+  }
+}
+</script>
