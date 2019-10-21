ed38325136f337166a4cfa2e1b89f2d27d931723
dinglun <dinglun@yy.com>
2019-10-21 16:23:31 +0800

    版本2

--- src/views/nothing/index.vue
+++ src/views/nothing/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="wrap">
     <h1 style="color: blue;">404</h1>
+    <h1 style="color: blue;">版本2</h1>
     <!-- <van-button type="danger" @click="routerPush">危险按钮</van-button>
     <func info="name" age="23" @clikcInfo="clikcInfo" @input.native="clikcInfo">
       <strong slot="strong" style="color: pink;">强壮</strong>
